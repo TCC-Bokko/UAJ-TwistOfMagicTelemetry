34d62c79e4f929dcacd764f01ffa476645c00cad
Antonio Cardona Costa <antocard@ucm.es>
2020-04-25 19:18:17 +0200

    Checkpoints en Nivel Tutorial

---
+++ Assets/scripts/TrackerSystem/LevelCheckpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCheckpoint : MonoBehaviour
+{
+    private Movimiento jugador;
+    bool triggered;
+    public int checkPoint_num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggered = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D otro)
+    {
+        if (otro.name == jugador.name && !triggered)
+        {
+            TrackerEvent checkpoint = new EventCheckpoint(checkPoint_num);
+            GM.TrackerInstance.TrackEvent(checkpoint);
+            triggered = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
